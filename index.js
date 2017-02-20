ce68fa99ba04dfcc3c56a045e00f3e8d2fe25e58
Denís Graña <denisgranha@gmail.com>
2017-02-20 20:10:49 +0100

    version 1.0.2 with support for logs

--- index.js
+++ index.js
@@ -16,17 +16,42 @@ function _addABI(abiArray) {
     // Iterate new abi to generate method id's
     abiArray.map((abi) => {
       if(abi.name){
-            const signature = new Web3().sha3(abi.name + "(" + abi.inputs.map(function(input) {return input.type;}).join(",") + ")");
-            if(abi.type == "event"){
-              state.methodIDs[signature.slice(2)] = abi;
-            }
-            else{
-              state.methodIDs[signature.slice(2, 10)] = abi;
-            }
-          }
+        const signature = new Web3().sha3(abi.name + "(" + abi.inputs.map(function(input) {return input.type;}).join(",") + ")");
+        if(abi.type == "event"){
+          state.methodIDs[signature.slice(2)] = abi;
+        }
+        else{
+          state.methodIDs[signature.slice(2, 10)] = abi;
+        }
+      }
     });
 
     state.savedABIs = state.savedABIs.concat(abiArray);
+  }
+  else {
+    throw new Error("Expected ABI array, got " + typeof abiArray);
+  }
+}
+
+function _removeABI(abiArray) {
+  if (Array.isArray(abiArray)) {
+
+    // Iterate new abi to generate method id's
+    abiArray.map((abi) => {
+      if(abi.name){
+        const signature = new Web3().sha3(abi.name + "(" + abi.inputs.map(function(input) {return input.type;}).join(",") + ")");
+        if(abi.type == "event"){
+          if (state.methodIDs[signature.slice(2)]) {
+            delete state.methodIDs[signature.slice(2)];
+          }
+        }
+        else{
+          if (state.methodIDs[signature.slice(2, 10)]) {
+            delete state.methodIDs[signature.slice(2, 10)];
+          }
+        }
+      }
+    });
   }
   else {
     throw new Error("Expected ABI array, got " + typeof abiArray);
@@ -38,7 +63,7 @@ function _getMethodIDs() {
 }
 
 function _decodeMethod(data) {
-  const methodID = data.slice(2, 10);  
+  const methodID = data.slice(2, 10);
   const abiItem = state.methodIDs[methodID];
   if (abiItem) {
     const params = abiItem.inputs.map((item) => item.type);
@@ -59,9 +84,55 @@ function _decodeMethod(data) {
   }
 }
 
+function _decodeLogs(logs) {
+  return logs.map((logItem) => {
+    const methodID = logItem.topics[0].slice(2);
+    const method = state.methodIDs[methodID];
+    if (method) {
+      const logData = logItem.data;
+      let decodedParams = [];
+      let dataIndex = 2;
+      let topicsIndex = 1;
+      // Loop topic and data to get the params
+      method.inputs.map(function (param) {
+        var decodedP = {
+          name: param.name
+        };
+
+        if (param.indexed) {
+          decodedP.value = logItem.topics[topicsIndex];
+          topicsIndex++;
+        }
+        else {
+          let dataEndIndex = dataIndex + 64;
+          decodedP.value = "0x" + logData.slice(dataIndex, dataEndIndex);
+          dataIndex = dataEndIndex;
+        }
+
+        if (param.type == "address"){
+          decodedP.value = "0x" + new Web3().toBigNumber(decodedP.value).toString(16);
+        }
+        else if(param.type == "uint256" || param.type == "uint8" || param.type == "int" ){
+          decodedP.value = new Web3().toBigNumber(decodedP.value).toString(10);
+        }
+
+        decodedParams.push(decodedP);
+      });
+
+
+      return {
+        name: method.name,
+        events: decodedParams
+      };
+    }
+  });
+}
+
 module.exports = {
   getABIs: _getABIs,
   addABI: _addABI,
   getMethodIDs: _getMethodIDs,
-  decodeMethod: _decodeMethod
+  decodeMethod: _decodeMethod,
+  decodeLogs: _decodeLogs,
+  removeABI: _removeABI
 };
--- test/test.js
+++ test/test.js
@@ -1,12 +1,8 @@
 const expect = require('chai').expect;
-let abiDecoder = null;
+const abiDecoder = require('../index.js');
 
 // Test Params
 const testABI = [{"inputs": [{"type": "address", "name": ""}], "constant": true, "name": "isInstantiation", "payable": false, "outputs": [{"type": "bool", "name": ""}], "type": "function"}, {"inputs": [{"type": "address[]", "name": "_owners"}, {"type": "uint256", "name": "_required"}, {"type": "uint256", "name": "_dailyLimit"}], "constant": false, "name": "create", "payable": false, "outputs": [{"type": "address", "name": "wallet"}], "type": "function"}, {"inputs": [{"type": "address", "name": ""}, {"type": "uint256", "name": ""}], "constant": true, "name": "instantiations", "payable": false, "outputs": [{"type": "address", "name": ""}], "type": "function"}, {"inputs": [{"type": "address", "name": "creator"}], "constant": true, "name": "getInstantiationCount", "payable": false, "outputs": [{"type": "uint256", "name": ""}], "type": "function"}, {"inputs": [{"indexed": false, "type": "address", "name": "sender"}, {"indexed": false, "type": "address", "name": "instantiation"}], "type": "event", "name": "ContractInstantiation", "anonymous": false}];
-
-beforeEach(() => {
-  abiDecoder = require('../index.js');
-});
 
 describe('abi decoder', function () {
   it('get abis', () => {
@@ -41,4 +37,56 @@ describe('abi decoder', function () {
     expect(decodedData.params[2].value).to.equal('0');
     expect(decodedData.params[2].name).to.equal('_dailyLimit');
   });
+
+  it('decode logs without indexed', () => {
+    const testLogs = [
+      {
+        data: "0x00000000000000000000000065039084cc6f4773291a6ed7dcf5bc3a2e894ff3000000000000000000000000435a4167bc34107bd03e267f9d6b869255151a27",
+        topics: ["0x4fb057ad4a26ed17a57957fa69c306f11987596069b89521c511fc9a894e6161"]
+      }
+    ];
+
+    const decodedLogs = abiDecoder.decodeLogs(testLogs);
+    expect(decodedLogs).to.be.an('array');
+    expect(decodedLogs).to.have.length(1);
+    expect(decodedLogs[0].name).to.equal('ContractInstantiation');
+    expect(decodedLogs[0].events).to.have.length(2);
+    expect(decodedLogs[0].events[0].name).to.equal('sender');
+    expect(decodedLogs[0].events[0].value).to.equal('0x65039084cc6f4773291a6ed7dcf5bc3a2e894ff3');
+    expect(decodedLogs[0].events[1].name).to.equal('instantiation');
+    expect(decodedLogs[0].events[1].value).to.equal('0x435a4167bc34107bd03e267f9d6b869255151a27');
+  });
+
+  it('decode logs with indexed param', () => {
+    const walletABI = [{"inputs": [{"type": "uint256", "name": ""}], "constant": true, "name": "owners", "payable": false, "outputs": [{"type": "address", "name": ""}], "type": "function"}, {"inputs": [{"type": "address", "name": "owner"}], "constant": false, "name": "removeOwner", "payable": false, "outputs": [], "type": "function"}, {"inputs": [{"type": "uint256", "name": "transactionId"}], "constant": false, "name": "revokeConfirmation", "payable": false, "outputs": [], "type": "function"}, {"inputs": [{"type": "address", "name": ""}], "constant": true, "name": "isOwner", "payable": false, "outputs": [{"type": "bool", "name": ""}], "type": "function"}, {"inputs": [{"type": "uint256", "name": ""}, {"type": "address", "name": ""}], "constant": true, "name": "confirmations", "payable": false, "outputs": [{"type": "bool", "name": ""}], "type": "function"}, {"inputs": [], "constant": true, "name": "calcMaxWithdraw", "payable": false, "outputs": [{"type": "uint256", "name": ""}], "type": "function"}, {"inputs": [{"type": "bool", "name": "pending"}, {"type": "bool", "name": "executed"}], "constant": true, "name": "getTransactionCount", "payable": false, "outputs": [{"type": "uint256", "name": "count"}], "type": "function"}, {"inputs": [], "constant": true, "name": "dailyLimit", "payable": false, "outputs": [{"type": "uint256", "name": ""}], "type": "function"}, {"inputs": [], "constant": true, "name": "lastDay", "payable": false, "outputs": [{"type": "uint256", "name": ""}], "type": "function"}, {"inputs": [{"type": "address", "name": "owner"}], "constant": false, "name": "addOwner", "payable": false, "outputs": [], "type": "function"}, {"inputs": [{"type": "uint256", "name": "transactionId"}], "constant": true, "name": "isConfirmed", "payable": false, "outputs": [{"type": "bool", "name": ""}], "type": "function"}, {"inputs": [{"type": "uint256", "name": "transactionId"}], "constant": true, "name": "getConfirmationCount", "payable": false, "outputs": [{"type": "uint256", "name": "count"}], "type": "function"}, {"inputs": [{"type": "uint256", "name": ""}], "constant": true, "name": "transactions", "payable": false, "outputs": [{"type": "address", "name": "destination"}, {"type": "uint256", "name": "value"}, {"type": "bytes", "name": "data"}, {"type": "bool", "name": "executed"}], "type": "function"}, {"inputs": [], "constant": true, "name": "getOwners", "payable": false, "outputs": [{"type": "address[]", "name": ""}], "type": "function"}, {"inputs": [{"type": "uint256", "name": "from"}, {"type": "uint256", "name": "to"}, {"type": "bool", "name": "pending"}, {"type": "bool", "name": "executed"}], "constant": true, "name": "getTransactionIds", "payable": false, "outputs": [{"type": "uint256[]", "name": "_transactionIds"}], "type": "function"}, {"inputs": [{"type": "uint256", "name": "transactionId"}], "constant": true, "name": "getConfirmations", "payable": false, "outputs": [{"type": "address[]", "name": "_confirmations"}], "type": "function"}, {"inputs": [], "constant": true, "name": "transactionCount", "payable": false, "outputs": [{"type": "uint256", "name": ""}], "type": "function"}, {"inputs": [{"type": "uint256", "name": "_required"}], "constant": false, "name": "changeRequirement", "payable": false, "outputs": [], "type": "function"}, {"inputs": [{"type": "uint256", "name": "transactionId"}], "constant": false, "name": "confirmTransaction", "payable": false, "outputs": [], "type": "function"}, {"inputs": [{"type": "address", "name": "destination"}, {"type": "uint256", "name": "value"}, {"type": "bytes", "name": "data"}], "constant": false, "name": "submitTransaction", "payable": false, "outputs": [{"type": "uint256", "name": "transactionId"}], "type": "function"}, {"inputs": [{"type": "uint256", "name": "_dailyLimit"}], "constant": false, "name": "changeDailyLimit", "payable": false, "outputs": [], "type": "function"}, {"inputs": [], "constant": true, "name": "MAX_OWNER_COUNT", "payable": false, "outputs": [{"type": "uint256", "name": ""}], "type": "function"}, {"inputs": [], "constant": true, "name": "required", "payable": false, "outputs": [{"type": "uint256", "name": ""}], "type": "function"}, {"inputs": [{"type": "address", "name": "owner"}, {"type": "address", "name": "newOwner"}], "constant": false, "name": "replaceOwner", "payable": false, "outputs": [], "type": "function"}, {"inputs": [{"type": "uint256", "name": "transactionId"}], "constant": false, "name": "executeTransaction", "payable": false, "outputs": [], "type": "function"}, {"inputs": [], "constant": true, "name": "spentToday", "payable": false, "outputs": [{"type": "uint256", "name": ""}], "type": "function"}, {"inputs": [{"type": "address[]", "name": "_owners"}, {"type": "uint256", "name": "_required"}, {"type": "uint256", "name": "_dailyLimit"}], "type": "constructor"}, {"payable": true, "type": "fallback"}, {"inputs": [{"indexed": false, "type": "uint256", "name": "dailyLimit"}], "type": "event", "name": "DailyLimitChange", "anonymous": false}, {"inputs": [{"indexed": true, "type": "address", "name": "sender"}, {"indexed": true, "type": "uint256", "name": "transactionId"}], "type": "event", "name": "Confirmation", "anonymous": false}, {"inputs": [{"indexed": true, "type": "address", "name": "sender"}, {"indexed": true, "type": "uint256", "name": "transactionId"}], "type": "event", "name": "Revocation", "anonymous": false}, {"inputs": [{"indexed": true, "type": "uint256", "name": "transactionId"}], "type": "event", "name": "Submission", "anonymous": false}, {"inputs": [{"indexed": true, "type": "uint256", "name": "transactionId"}], "type": "event", "name": "Execution", "anonymous": false}, {"inputs": [{"indexed": true, "type": "uint256", "name": "transactionId"}], "type": "event", "name": "ExecutionFailure", "anonymous": false}, {"inputs": [{"indexed": true, "type": "address", "name": "sender"}, {"indexed": false, "type": "uint256", "name": "value"}], "type": "event", "name": "Deposit", "anonymous": false}, {"inputs": [{"indexed": true, "type": "address", "name": "owner"}], "type": "event", "name": "OwnerAddition", "anonymous": false}, {"inputs": [{"indexed": true, "type": "address", "name": "owner"}], "type": "event", "name": "OwnerRemoval", "anonymous": false}, {"inputs": [{"indexed": false, "type": "uint256", "name": "required"}], "type": "event", "name": "RequirementChange", "anonymous": false}];
+    abiDecoder.addABI(walletABI);
+    const testLogs = [
+      {
+        data: "0x00000000000000000000000000000000000000000000000000038d7ea4c68000",
+        topics: ["0xe1fffcc4923d04b559f4d29a8bfc6cda04eb5b0d3c460751c2402c5c5cc9109c", "0x00000000000000000000000065039084cc6f4773291a6ed7dcf5bc3a2e894ff3"]
+      }
+    ];
+    const decodedLogs = abiDecoder.decodeLogs(testLogs);
+    expect(decodedLogs).to.be.an('array');
+    expect(decodedLogs).to.have.length(1);
+    expect(decodedLogs[0].name).to.equal('Deposit');
+    expect(decodedLogs[0].events).to.have.length(2);
+    expect(decodedLogs[0].events[0].name).to.equal('sender');
+    expect(decodedLogs[0].events[0].value).to.equal('0x65039084cc6f4773291a6ed7dcf5bc3a2e894ff3');
+    expect(decodedLogs[0].events[1].name).to.equal('value');
+    expect(decodedLogs[0].events[1].value).to.equal('1000000000000000');
+  });
+
+  it('remove ABI', () => {
+    let methods = abiDecoder.getMethodIDs();
+    expect(methods).to.be.an('object');
+    expect(Object.keys(methods)).to.have.length.of(41);
+
+    abiDecoder.removeABI(testABI);
+
+    methods = abiDecoder.getMethodIDs();
+    expect(methods).to.be.an('object');
+    expect(Object.keys(methods)).to.have.length.of(36);
+  });
+
 });
