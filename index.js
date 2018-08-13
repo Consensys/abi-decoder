54c0c07f74a5f094158bf9028165b1e9b7061aa7
Chris Rueber <crueber@gmail.com>
2018-08-13 17:46:16 -0500

    Need to have instances.

--- index.js
+++ index.js
@@ -1,172 +1,173 @@
 const SolidityCoder = require("web3/lib/solidity/coder.js");
 const Web3 = require('web3');
+const { sha3, toBigNumber } = new Web3();
 
-const state = {
-  savedABIs : [],
-  methodIDs: {}
-}
-
-function _getABIs() {
-  return state.savedABIs;
-}
-
-function _addABI(abiArray) {
-  if (Array.isArray(abiArray)) {
-
-    // Iterate new abi to generate method id's
-    abiArray.map(function (abi) {
-      if(abi.name){
-        const signature = new Web3().sha3(abi.name + "(" + abi.inputs.map(function(input) {return input.type;}).join(",") + ")");
-        if(abi.type == "event"){
-          state.methodIDs[signature.slice(2)] = abi;
-        }
-        else{
-          state.methodIDs[signature.slice(2, 10)] = abi;
-        }
-      }
-    });
-
-    state.savedABIs = state.savedABIs.concat(abiArray);
+module.exports = () => {
+  const state = { savedABIs: [], methodIDs: {} }
+  
+  function _getABIs() {
+    return state.savedABIs;
   }
-  else {
-    throw new Error("Expected ABI array, got " + typeof abiArray);
-  }
-}
-
-function _removeABI(abiArray) {
-  if (Array.isArray(abiArray)) {
-
-    // Iterate new abi to generate method id's
-    abiArray.map(function (abi) {
-      if(abi.name){
-        const signature = new Web3().sha3(abi.name + "(" + abi.inputs.map(function(input) {return input.type;}).join(",") + ")");
-        if(abi.type == "event"){
-          if (state.methodIDs[signature.slice(2)]) {
-            delete state.methodIDs[signature.slice(2)];
+  
+  function _addABI(abiArray) {
+    if (Array.isArray(abiArray)) {
+  
+      // Iterate new abi to generate method id's
+      abiArray.map(function (abi) {
+        if(abi.name){
+          const signature = sha3(abi.name + "(" + abi.inputs.map(function(input) {return input.type;}).join(",") + ")");
+          if(abi.type == "event"){
+            state.methodIDs[signature.slice(2)] = abi;
+          }
+          else{
+            state.methodIDs[signature.slice(2, 10)] = abi;
           }
         }
-        else{
-          if (state.methodIDs[signature.slice(2, 10)]) {
-            delete state.methodIDs[signature.slice(2, 10)];
-          }
-        }
-      }
-    });
-  }
-  else {
-    throw new Error("Expected ABI array, got " + typeof abiArray);
-  }
-}
-
-function _getMethodIDs() {
-  return state.methodIDs;
-}
-
-function _decodeMethod(data) {
-  const methodID = data.slice(2, 10);
-  const abiItem = state.methodIDs[methodID];
-  if (abiItem) {
-    const params = abiItem.inputs.map(function (item) { return item.type; });
-    let decoded = SolidityCoder.decodeParams(params, data.slice(10));
-    return {
-      name: abiItem.name,
-      params: decoded.map(function (param, index) {
-        let parsedParam = param;
-        const isUint = abiItem.inputs[index].type.indexOf("uint") == 0;
-        const isInt = abiItem.inputs[index].type.indexOf("int") == 0;
-
-        if (isUint || isInt) {
-          const isArray = Array.isArray(param);
-
-          if (isArray) {
-            parsedParam = param.map(val => new Web3().toBigNumber(val).toString());
-          } else {
-            parsedParam = new Web3().toBigNumber(param).toString();
-          }
-        }
-        return {
-          name: abiItem.inputs[index].name,
-          value: parsedParam,
-          type: abiItem.inputs[index].type
-        };
-      })
-    }
-  }
-}
-
-function padZeros (address) {
-  var formatted = address;
-  if (address.indexOf('0x') != -1) {
-    formatted = address.slice(2);
-  }
-
-  if (formatted.length < 40) {
-    while (formatted.length < 40) formatted = "0" + formatted;
-  }
-
-  return "0x" + formatted;
-};
-
-function _decodeLogs(logs) {
-  return logs.map(function(logItem) {
-    const methodID = logItem.topics[0].slice(2);
-    const method = state.methodIDs[methodID];
-    if (method) {
-      const logData = logItem.data;
-      let decodedParams = [];
-      let dataIndex = 0;
-      let topicsIndex = 1;
-
-      let dataTypes = [];
-      method.inputs.map(
-        function (input) {
-          if (!input.indexed) {
-            dataTypes.push(input.type);
-          }
-        }
-      );
-      const decodedData = SolidityCoder.decodeParams(dataTypes, logData.slice(2));
-      // Loop topic and data to get the params
-      method.inputs.map(function (param) {
-        var decodedP = {
-          name: param.name,
-          type: param.type
-        };
-
-        if (param.indexed) {
-          decodedP.value = logItem.topics[topicsIndex];
-          topicsIndex++;
-        }
-        else {
-          decodedP.value = decodedData[dataIndex];
-          dataIndex++;
-        }
-
-        if (param.type == "address"){
-          decodedP.value = padZeros(new Web3().toBigNumber(decodedP.value).toString(16));
-        }
-        else if(param.type == "uint256" || param.type == "uint8" || param.type == "int" ){
-          decodedP.value = new Web3().toBigNumber(decodedP.value).toString(10);
-        }
-
-        decodedParams.push(decodedP);
       });
-
-
-      return {
-        name: method.name,
-        events: decodedParams,
-        address: logItem.address
-      };
+  
+      state.savedABIs = state.savedABIs.concat(abiArray);
     }
-  });
-}
+    else {
+      throw new Error("Expected ABI array, got " + typeof abiArray);
+    }
 
-module.exports = {
-  getABIs: _getABIs,
-  addABI: _addABI,
-  getMethodIDs: _getMethodIDs,
-  decodeMethod: _decodeMethod,
-  decodeLogs: _decodeLogs,
-  removeABI: _removeABI
-};
+    return this;
+  }
+
+  function _removeABI(abiArray) {
+    if (Array.isArray(abiArray)) {
+  
+      // Iterate new abi to generate method id's
+      abiArray.map(function (abi) {
+        if(abi.name){
+          const signature = sha3(abi.name + "(" + abi.inputs.map(function(input) {return input.type;}).join(",") + ")");
+          if(abi.type == "event"){
+            if (state.methodIDs[signature.slice(2)]) {
+              delete state.methodIDs[signature.slice(2)];
+            }
+          }
+          else{
+            if (state.methodIDs[signature.slice(2, 10)]) {
+              delete state.methodIDs[signature.slice(2, 10)];
+            }
+          }
+        }
+      });
+    }
+    else {
+      throw new Error("Expected ABI array, got " + typeof abiArray);
+    }
+  }
+  
+  function _getMethodIDs() {
+    return state.methodIDs;
+  }
+  
+  function _decodeMethod(data) {
+    const methodID = data.slice(2, 10);
+    const abiItem = state.methodIDs[methodID];
+    if (abiItem) {
+      const params = abiItem.inputs.map(function (item) { return item.type; });
+      let decoded = SolidityCoder.decodeParams(params, data.slice(10));
+      return {
+        name: abiItem.name,
+        params: decoded.map(function (param, index) {
+          let parsedParam = param;
+          const isUint = abiItem.inputs[index].type.indexOf("uint") == 0;
+          const isInt = abiItem.inputs[index].type.indexOf("int") == 0;
+  
+          if (isUint || isInt) {
+            const isArray = Array.isArray(param);
+  
+            if (isArray) {
+              parsedParam = param.map(val => toBigNumber(val).toString());
+            } else {
+              parsedParam = toBigNumber(param).toString();
+            }
+          }
+          return {
+            name: abiItem.inputs[index].name,
+            value: parsedParam,
+            type: abiItem.inputs[index].type
+          };
+        })
+      }
+    }
+  }
+  
+  function padZeros (address) {
+    var formatted = address;
+    if (address.indexOf('0x') != -1) {
+      formatted = address.slice(2);
+    }
+  
+    if (formatted.length < 40) {
+      while (formatted.length < 40) formatted = "0" + formatted;
+    }
+  
+    return "0x" + formatted;
+  };
+  
+  function _decodeLogs(logs) {
+    return logs.map(function(logItem) {
+      const methodID = logItem.topics[0].slice(2);
+      const method = state.methodIDs[methodID];
+      if (method) {
+        const logData = logItem.data;
+        let decodedParams = [];
+        let dataIndex = 0;
+        let topicsIndex = 1;
+  
+        let dataTypes = [];
+        method.inputs.map(
+          function (input) {
+            if (!input.indexed) {
+              dataTypes.push(input.type);
+            }
+          }
+        );
+        const decodedData = SolidityCoder.decodeParams(dataTypes, logData.slice(2));
+        // Loop topic and data to get the params
+        method.inputs.map(function (param) {
+          var decodedP = {
+            name: param.name,
+            type: param.type
+          };
+  
+          if (param.indexed) {
+            decodedP.value = logItem.topics[topicsIndex];
+            topicsIndex++;
+          }
+          else {
+            decodedP.value = decodedData[dataIndex];
+            dataIndex++;
+          }
+  
+          if (param.type == "address"){
+            decodedP.value = padZeros(toBigNumber(decodedP.value).toString(16));
+          }
+          else if(param.type == "uint256" || param.type == "uint8" || param.type == "int" ){
+            decodedP.value = toBigNumber(decodedP.value).toString(10);
+          }
+  
+          decodedParams.push(decodedP);
+        });
+  
+        return {
+          name: method.name,
+          events: decodedParams,
+          address: logItem.address
+        };
+      }
+    });
+  }  
+
+  return {
+    getABIs: _getABIs,
+    addABI: _addABI,
+    getMethodIDs: _getMethodIDs,
+    decodeMethod: _decodeMethod,
+    decodeLogs: _decodeLogs,
+    removeABI: _removeABI
+  };
+}
--- test/test.js
+++ test/test.js
@@ -1,8 +1,12 @@
 const expect = require('chai').expect;
-const abiDecoder = require('../index.js');
+const AbiDecoder = require('../index.js');
+const abiDecoder = AbiDecoder();
 
 // Test Params
 const testABI = [{"inputs": [{"type": "address", "name": ""}], "constant": true, "name": "isInstantiation", "payable": false, "outputs": [{"type": "bool", "name": ""}], "type": "function"}, {"inputs": [{"type": "address[]", "name": "_owners"}, {"type": "uint256", "name": "_required"}, {"type": "uint256", "name": "_dailyLimit"}], "constant": false, "name": "create", "payable": false, "outputs": [{"type": "address", "name": "wallet"}], "type": "function"}, {"inputs": [{"type": "address", "name": ""}, {"type": "uint256", "name": ""}], "constant": true, "name": "instantiations", "payable": false, "outputs": [{"type": "address", "name": ""}], "type": "function"}, {"inputs": [{"type": "address", "name": "creator"}], "constant": true, "name": "getInstantiationCount", "payable": false, "outputs": [{"type": "uint256", "name": ""}], "type": "function"}, {"inputs": [{"indexed": false, "type": "address", "name": "sender"}, {"indexed": false, "type": "address", "name": "instantiation"}], "type": "event", "name": "ContractInstantiation", "anonymous": false}];
+
+const testABI2 = [{"inputs": [{"type": "address[]", "name": "_owners"}, {"type": "uint256", "name": "_required"}, {"type": "uint256", "name": "_dailyLimit"}], "constant": false, "name": "create", "payable": false, "outputs": [{"type": "address", "name": "wallet"}], "type": "function"}, {"inputs": [{"type": "address", "name": "creator"}], "constant": true, "name": "getInstantiationCount", "payable": false, "outputs": [{"type": "uint256", "name": ""}], "type": "function"}, {"inputs": [{"indexed": false, "type": "address", "name": "sender"}, {"indexed": false, "type": "address", "name": "instantiation"}], "type": "event", "name": "ContractInstantiation", "anonymous": false}];
+
 const testArrNumbersABI = [{"constant":false,"inputs":[{"name":"n","type":"uint256[]"}],"name":"numbers","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"function"}];
 
 describe('abi decoder', function () {
@@ -20,6 +24,17 @@ describe('abi decoder', function () {
     const methodIDs = abiDecoder.getMethodIDs();
     expect(methodIDs).to.be.an('object');
     expect(Object.keys(methodIDs)).to.have.length.of(5);
+  });
+
+  it('add second abi', () => {
+    const ad = AbiDecoder()
+    ad.addABI(testABI2);
+    const abis = ad.getABIs();
+    expect(abis).to.be.an('array');
+    expect(abis).to.have.length.of(3);
+    const methodIDs = ad.getMethodIDs();
+    expect(methodIDs).to.be.an('object');
+    expect(Object.keys(methodIDs)).to.have.length.of(3);
   });
 
   it('decode data', () => {
