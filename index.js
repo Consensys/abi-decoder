6d342334ff5cc2c460846f02a895d5436e18f131
Chris Cates <hello@chriscates.ca>
2022-10-25 20:51:17 -0400

    Fix critical BN.js typecast bug

--- index.js
+++ index.js
@@ -183,13 +183,11 @@ function _decodeLogs(logs) {
           param.type === "uint8" ||
           param.type === "int"
         ) {
-          // ensure to remove leading 0x for hex numbers
           if (typeof decodedP.value === "string" && decodedP.value.startsWith("0x")) {
-            decodedP.value = new BN(decodedP.value.slice(2), 16).toString(10);
+            decodedP.value = new BN(decodedP.value, 16).toString(10);
           } else {
             decodedP.value = new BN(decodedP.value).toString(10);
           }
-
         }
 
         decodedParams.push(decodedP);
